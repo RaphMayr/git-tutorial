419c43944001690093ffd463b69e6fcd72acc63d
Raphael Mayr <raphael.mayr@tum.de>
2020-05-02 14:29:45 +0200

    Completed Coding Challenge 2

--- src/main.c
+++ src/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <espl_lib.h>
 
 int main()
-{
-	printf("Hello ESPL");
+{	
+	char menu;	
+	unsigned int input;
+	char* output;
+
+	menu = 'y';
+	
+	printf("Welcome!\n");
+
+	while(menu == 'y')
+	{ 
+		printf("Please insert a number:");
+		scanf("%d", &input);
+		
+		output = num_to_words(input);
+		printf("corresponding Word:%s\n", output);
+
+		printf("Would you like to insert another number ? (y/n):");
+		scanf(" %c", &menu);
+	}
+
+	printf("Until next time.\n");
+
 }
