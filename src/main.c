6c0f100e615e198b223515d1ae0672b2b332b99c
Raphael Mayr <raphael.mayr@tum.de>
2020-05-04 18:16:00 +0200

    resolved conflicts in main.c and my_states.c; program works correct now.

--- src/main.c
+++ src/main.c
@@ -64,6 +64,12 @@ void errno_abort(char *message) {
   exit(EXIT_FAILURE);
 }
 
+int err_abort(int status, char *message) {
+  fprintf(stderr, "%s\n", message);
+  exit(status);
+  return 0;
+}
+
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
   arguments_t *arguments = state->input;
 
@@ -177,20 +183,13 @@ int main(int argc, char **argv) {
          arguments.verbose ? "yes" : "no", arguments.tick);
 
   /** Initialize state machine */
-  /*
-  states_add(state_probe, state_two_enter, state_two_run, state_two_ext,
-             state_second_e, SECOND_STATE_NAME);
-  states_add(state_probe, NULL, state_three_run, NULL, state_third_e,
-             THIRD_STATE_NAME);
-  states_add(state_probe, NULL, state_one_run, NULL, state_first_e,
-             FIRST_STATE_NAME);
-  */
-  states_add(timer_callback, NULL, state_one_run, NULL, state_first_x,
-             FIRST_STATE_NAME);
-  states_add(state_probe, state_two_enter, state_two_run, state_two_exit,
-             state_second_e, SECOND_STATE_NAME);
-  states_add(state_probe, NULL, state_three_run, NULL, state_third_e,
-             THIRD_STATE_NAME);
+
+  states_add(state_probe, NULL, state_one_run, 
+	           NULL , state_first_e, FIRST_STATE_NAME);
+  states_add(state_probe, state_two_enter, state_two_run,
+	           state_two_exit, state_second_e, SECOND_STATE_NAME);
+  states_add(state_probe,NULL, state_three_run, 
+	           NULL, state_third_e, THIRD_STATE_NAME);
 
   states_set_callback(statemachine_callback);
 
@@ -222,8 +221,4 @@ int main(int argc, char **argv) {
   return -1;
 }
 
-int err_abort(int status, char *message) {
-  fprintf(stderr, "%s\n", message);
-  exit(status);
-  return 0;
-}
+
